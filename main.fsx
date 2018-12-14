1cbb5327d1d884279170d6d38a63691fa5275904
emil <emilbh@gmail.com>
2018-12-14 11:45:51 +0100

    made it posible to output the file with a name

--- main.fsx
+++ main.fsx
@@ -13,6 +13,7 @@ open System.IO
 /// <param name="fulv"> fulv is the time to breed a new wolf</param>
 /// <param name="s"> s represents hunger for the wolves before starvation</param>
 /// <param name="v"> v is a bool, verbose true or false. Default is false</param>
+/// <param name="out"> out is a bool, verbose true or false. Default is false</param>
 /// <remarks> Would have been smart if the first argument or
 /// something was a name of a text file to write to </remarks>
 /// <returns> A text file called test.txt with results </returns>
@@ -20,6 +21,17 @@ open System.IO
 let main args =
     let mutable str = "Wolf,  Moose\n"
     match args with
+    // With 9 arguments
+    | [|T; n; e; felg; u; fulv; s; v;out|] ->
+        let isle = animals.environment(int32(n), int32(e), int32(felg), int32(u), int32(fulv), int32(s), Convert.ToBoolean(v))
+        for i in 0..int32(T) do
+            str <- str + sprintf "%3s, %3s\n" (string(isle.board.wolves.Length)) (string(isle.board.moose.Length))
+            if (Convert.ToBoolean(v)) then
+                printfn "In the year %d" i
+                printfn "%A" isle
+            isle.tick ()
+        File.WriteAllText(string(out), str)
+    
     // With 8 arguments
     | [|T; n; e; felg; u; fulv; s; v|] ->
         let isle = animals.environment(int32(n), int32(e), int32(felg), int32(u), int32(fulv), int32(s), Convert.ToBoolean(v))
